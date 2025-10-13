4441d8dbc46995bb050ba2af6eabeac4157d2785
kieran-smith-amb <kieran.smith@ambulantsolutions.co.uk>
2025-10-13 23:56:26 +0100

    Load all modukes

--- src/itflow_api/itflow_api.py
+++ src/itflow_api/itflow_api.py
@@ -1,12 +1,22 @@
 import logging
 from .rest_adapter import RestAdapter
 from .exceptions import ITFlowApiException
-from .modules.clients import ClientsModule
-from .modules.contacts import ContactsModule
 from .modules.assets import AssetsModule
 from .modules.certificates import CertificatesModule
+from .modules.clients import ClientsModule
+from .modules.contacts import ContactsModule
 from .modules.credentials import CredentialsModule
 from .modules.documents import DocumentsModule
+from .modules.domains import DomainsModule
+from .modules.expenses import ExpensesModule
+from .modules.invoices import InvoicesModule
+from .modules.locations import LocationsModule
+from .modules.networks import NetworksModule
+from .modules.payments import PaymentsModule
+from .modules.products import ProductsModule
+from .modules.software import SoftwareModule
+from .modules.tickets import TicketsModule
+from .modules.vendors import VendorsModule
 
 class ITFlowApi:
     def __init__(self, hostname: str, api_key: str, ver: str = 'v1', ssl_verify: bool = True, logger: logging.Logger = None):
@@ -18,4 +28,14 @@ class ITFlowApi:
         self.contacts = ContactsModule(self._rest_adapter)
         self.credentials = CredentialsModule(self._rest_adapter)
         self.documents = DocumentsModule(self._rest_adapter)
+        self.domains = DomainsModule(self._rest_adapter)
+        self.expenses = ExpensesModule(self._rest_adapter)
+        self.invoices = InvoicesModule(self._rest_adapter)
+        self.locations = LocationsModule(self._rest_adapter)
+        self.networks = NetworksModule(self._rest_adapter)
+        self.payments = PaymentsModule(self._rest_adapter)
+        self.products = ProductsModule(self._rest_adapter)
+        self.software = SoftwareModule(self._rest_adapter)
+        self.tickets = TicketsModule(self._rest_adapter)
+        self.vendors = VendorsModule(self._rest_adapter)
         
--- src/itflow_api/modules/products.py
+++ src/itflow_api/modules/products.py
@@ -2,6 +2,6 @@ from ..models import Client
 from ..exceptions import ITFlowApiException
 from typing import List
 
-class ProductssModule:
+class ProductsModule:
     def __init__(self, rest_adapter):
         self._rest_adapter = rest_adapter
